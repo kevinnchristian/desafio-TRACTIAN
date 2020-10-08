2cb5c9f1887cf3f0eb0da17835b261473818797d
kevinnchristian <kevin.cmribeiro@outlook.com>
2020-10-08 16:41:49 -0300

    :art: Improve v2 controller company

--- backend/src/controllers/companyController.js
+++ backend/src/controllers/companyController.js
@@ -4,18 +4,21 @@ const CompanyController = {
   index: async (_req, res) => {
     try {
       const companies = await Company.find();
+
       return res.status(200).json(companies);
-
     } catch (err) {
-      return res.status(400).json({ msg: "Not registration" });
-
+      return res.status(400).json({ 
+        error: true,
+        msg: "Not registration" 
+      });
     }
   },
 
   create: async (req, res) => {
     const { company_name, maintenanceManager } = req.body;
 
-    const newCompany = await Company.create({
+    try {
+      const newCompany = await Company.create({
         company_name,
         maintenanceManager,
         units: [],
@@ -30,48 +33,65 @@ const CompanyController = {
             console.log(`⚠️  Error: ${err.name} - 💬 Message: ${err.messageFormat}`);
         }
       })
-
+    } catch (err) {
+      return res.status(400).json({ 
+        error: true,
+        msg: "Error create" 
+      });
+    } 
   },
 
   update: async (req, res) => {
     const { id } = req.params;
     const { company_name, maintenanceManager } = req.body;
     
-    const updateCompany = await Company.findByIdAndUpdate(id, {
-      company_name,
-      maintenanceManager,
-    }, { new: true })
-    .then(result => {
-      return res.status(201).json(result);
-    })
-    .catch(err => {
-      if (err) {
-        return res.status(400).json({
-            msg: "Company not found",
-          }) && 
-          console.log(`⚠️  Error: ${err.name} - 💬 Message: ${err.messageFormat}`);
-      }
-    });
-
+    try {
+      const updateCompany = await Company.findByIdAndUpdate(id, {
+        company_name,
+        maintenanceManager,
+      }, { new: true })
+      .then(result => {
+        return res.status(201).json(result);
+      })
+      .catch(err => {
+        if (err) {
+          return res.status(400).json({
+              msg: "Company not found",
+            }) && 
+            console.log(`⚠️  Error: ${err.name} - 💬 Message: ${err.messageFormat}`);
+        }
+      });
+    } catch (err) {
+      return res.status(400).json({ 
+        error: true,
+        msg: "Error in update" 
+      });
+    }
   },
 
   destroy: async (req, res) => {
     const { id } = req.params;
 
-    const deleteCompany = await Company.findByIdAndDelete(id)
-    .then(result => {
-      return res.sendStatus(204);
-    })
-    .catch(err => {
-      if (err) {
-        return res.status(400).json({
-            msg: "Company not found",
-          }) && 
-          console.log(`⚠️  Error: ${err.name} - 💬 Message: ${err.messageFormat}`);
-      }
-    });
-
-  },
+    try {
+      const deleteCompany = await Company.findByIdAndDelete(id)
+      .then(result => {
+        return res.sendStatus(204);
+      })
+      .catch(err => {
+        if (err) {
+          return res.status(400).json({
+              msg: "Company not found",
+            }) && 
+            console.log(`⚠️  Error: ${err.name} - 💬 Message: ${err.messageFormat}`);
+        }
+      });
+    } catch (err) {
+      return res.status(400).json({ 
+        error: true,
+        msg: "Error in delete" 
+      });
+    }
+  }
 
 };
 
